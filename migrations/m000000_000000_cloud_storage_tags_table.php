c3039f462f97200e43ca1ae996837b87c9f82296
alxlapin <1994enpnz58>
2022-04-11 22:29:29 +0300

    (issue #15) add migration for creating sys_cloud_storage_tags table

---
+++ migrations/m000000_000000_cloud_storage_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+use cusodede\s3\models\cloud_storage\active_record\CloudStorageAR;
+use cusodede\s3\models\cloud_storage\active_record\CloudStorageTagsAR;
+use yii\db\Migration;
+
+class m000000_000000_cloud_storage_tags_table extends Migration {
+    /**
+     * @return string
+     */
+    public static function mainTableName():string {
+        return CloudStorageTagsAR::tableName();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp() {
+        $this->createTable(self::mainTableName(), [
+            'id' => $this->primaryKey(),
+            'cloud_storage_id' => $this->integer()->notNull()->comment('ID загруженного файла'),
+            'tag_label' => $this->string()->notNull()->comment('Название тега'),
+            'tag_key' => $this->string()->notNull()->comment('Значение тега')
+        ]);
+
+        $this->addForeignKey('fk_storage_tag_to_storage', self::mainTableName(), 'cloud_storage_id', CloudStorageAR::tableName(), 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown() {
+        $this->dropTable(self::mainTableName());
+    }
+}
