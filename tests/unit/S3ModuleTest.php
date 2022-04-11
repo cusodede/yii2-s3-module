c5f4bd375ad9484a585f19e67d084af40627748a
alxlapin <1994enpnz58>
2022-04-11 22:41:14 +0300

    (issue #15) add testTagsBinding to S3ModuleTest.php

--- tests/unit/S3ModuleTest.php
+++ tests/unit/S3ModuleTest.php
@@ -4,10 +4,13 @@ declare(strict_types = 1);
 namespace unit;
 
 use Codeception\Test\Unit;
+use cusodede\s3\components\PutObjectMethodParams;
 use cusodede\s3\helpers\S3Helper;
+use cusodede\s3\models\S3;
 use Throwable;
 use Yii;
 use yii\base\Exception;
+use yii\helpers\ArrayHelper;
 
 /**
  * Class S3ModuleTest
@@ -29,4 +32,28 @@ class S3ModuleTest extends Unit {
 
 	}
 
+    /**
+     * @return void
+     * @throws Exception
+     * @throws Throwable
+     */
+    public function testTagsBinding():void {
+        $params = new PutObjectMethodParams();
+        $params->addTag('tag1', 'tag1value');
+        $params->addTag('tag2');
+
+        $storage = S3Helper::FileToStorage(Yii::getAlias(self::SAMPLE_FILE_PATH), null, null, $params);
+
+        $result = (new S3())->getObjectTagging($storage->key);
+
+        $tagSet = ArrayHelper::map($result->get('TagSet'), 'Key', 'Value');
+
+        $this::assertEquals('tag1value', $tagSet['tag1'] ?? '');
+        $this::assertEquals('tag2', $tagSet['tag2'] ?? '');
+
+        $tags = ArrayHelper::map($storage->relatedTags, 'tag_label', 'tag_key');
+
+        $this::assertArrayHasKey('tag1', $tags);
+        $this::assertArrayHasKey('tag2', $tags);
+    }
 }
