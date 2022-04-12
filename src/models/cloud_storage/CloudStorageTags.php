eb54ee7a65d1c9aa12503316f090c10938c1772c
Pozitronik <antikillerxxl@gmail.com>
2022-04-12 11:23:36 +0300

    #feature/tags Стабы todo-функций

--- src/models/cloud_storage/CloudStorageTags.php
+++ src/models/cloud_storage/CloudStorageTags.php
@@ -5,11 +5,41 @@ namespace cusodede\s3\models\cloud_storage;
 
 use cusodede\s3\models\cloud_storage\active_record\CloudStorageTagsAR;
 
-/** @noinspection EmptyClassInspection на будущее, чтобы related атрибуты завязывались не на AR модель */
-
 /**
  * Class CloudStorageTags
  */
 class CloudStorageTags extends CloudStorageTagsAR {
+
+	/**
+	 * Получить все теги всех записей из S3
+	 * @return void
+	 */
+	public static function SyncAllFromS3():void {
+		//todo
+	}
+
+	/**
+	 * Записать все теги всех записей в S3
+	 * @return void
+	 */
+	public static function SyncAllToS3():void {
+		//todo
+	}
+
+	/**
+	 * Взять теги записи из S3
+	 * @return void
+	 */
+	public function syncFromS3():void {
+		//todo
+	}
+
+	/**
+	 * Записать теги записи в S3
+	 * @return void
+	 */
+	public function syncToS3():void {
+		//todo
+	}
 
 }
