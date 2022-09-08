dd509253c6c8269ba28a646200015b12d5cc3ea5
Pozitronik <antikillerxxl@gmail.com>
2022-09-08 17:06:51 +0300

    migration rollback fixed

--- migrations/m000000_000003_cloud_storage_timestamps_timezones.php
+++ migrations/m000000_000003_cloud_storage_timestamps_timezones.php
@@ -36,7 +36,7 @@ class m000000_000003_cloud_storage_timestamps_timezones extends Migration {
 	 */
 	public function safeDown() {
 		if (null !== ArrayHelper::getValue($this->db->schema->typeMap, 'timestamptz')) {
-			$this->alterColumn(self::mainTableName(), 'created_at', $this->timestamptz(0)->notNull()->comment('Дата и время создания.'));
+			$this->alterColumn(self::mainTableName(), 'created_at', $this->timestamp(0)->notNull()->comment('Дата и время создания.'));
 		} else {
 			Yii::info('timestamptz column type is not supported bu DB schema, migration not applied.');
 		}
