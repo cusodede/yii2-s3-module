712a00d567a6ca233621665bee18e570201ed85c
Pozitronik <antikillerxxl@gmail.com>
2025-08-14 22:57:58 +0400

    Replace outdated custom Yii2 module with _before() method && update test suites configs

--- tests/_support/Helper/Functional.php
+++ tests/_support/Helper/Functional.php
@@ -1,10 +1,15 @@
 <?php
+declare(strict_types = 1);
+
 namespace Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class Functional extends \Codeception\Module
-{
+use Codeception\Module;
 
+/**
+ * class Functional
+ */
+class Functional extends Module {
 }
