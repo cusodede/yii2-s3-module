deb17f9ea0bbcd7f5930e5da411262bfcd21319c
Moiseenko <s1lverhron@gmail.com>
2026-04-28 13:15:57 +0300

    Code style

--- src/models/ArrayTagAdapter.php
+++ src/models/ArrayTagAdapter.php
@@ -1,77 +1,84 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace cusodede\s3\models;
 
 /**
  * Адаптер для работы с тегами, как с массивом ключ-значение
  */
-class ArrayTagAdapter {
+class ArrayTagAdapter
+{
+    /**
+     * @var string[]
+     */
+    private array $_tags = [];
 
-	/**
-	 * @var string[]
-	 */
-	private array $_tags = [];
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return http_build_query($this->_tags);
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString():string {
-		return http_build_query($this->_tags);
-	}
+    /**
+     * @param null|string[] $tags
+     */
+    public function __construct(?array $tags = null)
+    {
+        if (null !== $tags) {
+            foreach ($tags as $tag => $value) {
+                $this->setTag(is_string($tag)?$tag:$value, $value);
+            }
+        }
+    }
 
-	/**
-	 * @param null|string[] $tags
-	 */
-	public function __construct(?array $tags = null) {
-		if (null !== $tags) {
-			foreach ($tags as $tag => $value) {
-				$this->setTag(is_string($tag)?$tag:$value, $value);
-			}
-		}
-	}
+    /**
+     * Устанавливает название и значение тега.
+     * @param string $name
+     * @param string|null $value Если `null`, то в качестве значения будет использовано название тега.
+     * @return void
+     */
+    public function setTag(string $name, ?string $value = null): void
+    {
+        $this->_tags[$name] = $value??$name;
+    }
 
-	/**
-	 * Устанавливает название и значение тега.
-	 * @param string $name
-	 * @param string|null $value Если `null`, то в качестве значения будет использовано название тега.
-	 * @return void
-	 */
-	public function setTag(string $name, ?string $value = null):void {
-		$this->_tags[$name] = $value??$name;
-	}
+    /**
+     * @return string[]
+     */
+    public function getTags(): array
+    {
+        return $this->_tags;
+    }
 
-	/**
-	 * @return string[]
-	 */
-	public function getTags():array {
-		return $this->_tags;
-	}
+    /**
+     * Добавляет тег, если он ещё не установлен
+     * @param string $name
+     * @param string|null $value Если `null`, то в качестве значения будет использовано название тега.
+     * @return bool True, если тег установлен
+     */
+    public function addTag(string $name, ?string $value = null): bool
+    {
+        if (isset($this->_tags[$name])) return false;
+        $this->setTag($name, $value);
+        return true;
+    }
 
-	/**
-	 * Добавляет тег, если он ещё не установлен
-	 * @param string $name
-	 * @param string|null $value Если `null`, то в качестве значения будет использовано название тега.
-	 * @return bool True, если тег установлен
-	 */
-	public function addTag(string $name, ?string $value = null):bool {
-		if (isset($this->_tags[$name])) return false;
-		$this->setTag($name, $value);
-		return true;
-	}
-
-	/**
-	 * Формат тегов для putObjectTagging
-	 * @return array
-	 */
-	public function tagSet():array {
-		$result = [];
-		foreach ($this->_tags as $key => $value) {
-			$result[] = [
-				'Key' => $key,
-				'Value' => $value
-			];
-		}
-		return $result;
-	}
+    /**
+     * Формат тегов для putObjectTagging
+     * @return array
+     */
+    public function tagSet(): array
+    {
+        $result = [];
+        foreach ($this->_tags as $key => $value) {
+            $result[] = [
+                'Key' => $key,
+                'Value' => $value
+            ];
+        }
+        return $result;
+    }
 }
--- src/models/cloud_storage/CloudStorageSearch.php
+++ src/models/cloud_storage/CloudStorageSearch.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace cusodede\s3\models\cloud_storage;
 
@@ -10,98 +11,102 @@ use yii\data\ActiveDataProvider;
 /**
  * Class CloudStorageSearch
  */
-class CloudStorageSearch extends CloudStorage {
+class CloudStorageSearch extends CloudStorage
+{
+    /**
+     * @var null|string $tagsFilter Фильтр по значению тега
+     */
+    public ?string $tagsFilter = null;
 
-	/**
-	 * @var null|string $tagsFilter Фильтр по значению тега
-	 */
-	public ?string $tagsFilter = null;
+    /**
+     * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [['id', 'model_key',], 'integer'],
+            [['uploaded', 'deleted'], 'boolean'],
+            [['bucket', 'key', 'filename', 'model_name', 'connection'], 'string', 'max' => 255],
+            [['tagsFilter'], 'string']
+        ];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function rules():array {
-		return [
-			[['id', 'model_key',], 'integer'],
-			[['uploaded', 'deleted'], 'boolean'],
-			[['bucket', 'key', 'filename', 'model_name', 'connection'], 'string', 'max' => 255],
-			[['tagsFilter'], 'string']
-		];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels(): array
+    {
+        return array_merge(
+            parent::attributeLabels(),
+            ['tagsFilter' => 'Теги']
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function attributeLabels():array {
-		return array_merge(
-			parent::attributeLabels(),
-			['tagsFilter' => 'Теги']
-		);
-	}
+    /**
+     * @param array $params
+     * @return ActiveDataProvider
+     * @throws Throwable
+     */
+    public function search(array $params): ActiveDataProvider
+    {
+        $query = self::find()->active();
+        $query->joinWith(['relatedTags']);
 
-	/**
-	 * @param array $params
-	 * @return ActiveDataProvider
-	 * @throws Throwable
-	 */
-	public function search(array $params):ActiveDataProvider {
-		$query = self::find()->active();
-		$query->joinWith(['relatedTags']);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query
+        ]);
 
-		$dataProvider = new ActiveDataProvider([
-			'query' => $query
-		]);
+        $this->setSort($dataProvider);
+        $this->load($params);
 
-		$this->setSort($dataProvider);
-		$this->load($params);
+        if (!$this->validate()) return $dataProvider;
 
-		if (!$this->validate()) return $dataProvider;
+        $this->filterData($query);
 
-		$this->filterData($query);
+        return $dataProvider;
+    }
 
-		return $dataProvider;
-	}
+    /**
+     * @param $query
+     * @return void
+     * @throws Throwable
+     */
+    private function filterData($query): void
+    {
+        $query->andFilterWhere([self::fieldName('id') => $this->id]);
+        $query->andFilterWhere([self::fieldName('deleted') => $this->deleted]);
+        $query->andFilterWhere([self::fieldName('uploaded') => $this->uploaded]);
+        $query->andFilterWhere(['like', self::fieldName('filename'), $this->filename]);
+        $query->andFilterWhere(['like', self::fieldName('bucket'), $this->bucket]);
+        $query->andFilterWhere(['like', self::fieldName('key'), $this->key]);
+        $query->andFilterWhere([self::fieldName('model_name') => $this->model_name]);
+        $query->andFilterWhere([self::fieldName('model_key') => $this->model_key]);
+        $query->andFilterWhere([self::fieldName('connection') => $this->connection]);
+        $query->andFilterWhere(['like', CloudStorageTagsAR::fieldName('tag_key'), $this->tagsFilter]);
+    }
 
-	/**
-	 * @param $query
-	 * @return void
-	 * @throws Throwable
-	 */
-	private function filterData($query):void {
-		$query->andFilterWhere([self::fieldName('id') => $this->id]);
-		$query->andFilterWhere([self::fieldName('deleted') => $this->deleted]);
-		$query->andFilterWhere([self::fieldName('uploaded') => $this->uploaded]);
-		$query->andFilterWhere(['like', self::fieldName('filename'), $this->filename]);
-		$query->andFilterWhere(['like', self::fieldName('bucket'), $this->bucket]);
-		$query->andFilterWhere(['like', self::fieldName('key'), $this->key]);
-		$query->andFilterWhere([self::fieldName('model_name') => $this->model_name]);
-		$query->andFilterWhere([self::fieldName('model_key') => $this->model_key]);
-		$query->andFilterWhere([self::fieldName('connection') => $this->connection]);
-		$query->andFilterWhere(['like', CloudStorageTagsAR::fieldName('tag_key'), $this->tagsFilter]);
-	}
-
-	/**
-	 * @param $dataProvider
-	 */
-	private function setSort($dataProvider):void {
-		$dataProvider->setSort([
-			'defaultOrder' => ['id' => SORT_DESC],
-			'attributes' => [
-				'id',
-				'deleted',
-				'uploaded',
-				'key',
-				'filename',
-				'bucket',
-				'model_name',
-				'model_key',
-				'connection',
-				'tagsFilter' => [
-					'asc' => [CloudStorageTagsAR::tableName().'.tag_key' => SORT_ASC],
-					'desc' => [CloudStorageTagsAR::tableName().'.tag_key' => SORT_DESC]
-				]
-			]
-		]);
-	}
-
+    /**
+     * @param $dataProvider
+     */
+    private function setSort($dataProvider): void
+    {
+        $dataProvider->setSort([
+            'defaultOrder' => ['id' => SORT_DESC],
+            'attributes' => [
+                'id',
+                'deleted',
+                'uploaded',
+                'key',
+                'filename',
+                'bucket',
+                'model_name',
+                'model_key',
+                'connection',
+                'tagsFilter' => [
+                    'asc' => [CloudStorageTagsAR::tableName().'.tag_key' => SORT_ASC],
+                    'desc' => [CloudStorageTagsAR::tableName().'.tag_key' => SORT_DESC]
+                ]
+            ]
+        ]);
+    }
 }
